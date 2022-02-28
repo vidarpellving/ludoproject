b1ff1e1be3e7d1243e4c3ca20b6654b117b7e106
Sigurd Drivenes <sigurd@student-228-217.eduroam.uu.se>
2022-02-28 15:31:06 +0100

    Added a click event function

--- Main.hs
+++ Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
 import Data.Array
+import Graphics.Gloss.Interface.IO.Game (Event(EventKey), MouseButton (LeftButton))
+import Graphics.Gloss.Interface.Pure.Game
+
 
 --player
 data Player = PlayerRed | PlayerBlue | PlayerYellow | PlayerGreen deriving (Eq, Show)
@@ -43,6 +46,14 @@ cellHeight = fromIntegral screenHeight / fromIntegral n
 n :: Int
 n = 15
 {- this is the startboard-}
+
+{-
+validPositions = [(0,8),(0,7),(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(6,5),(6,4),(6,3),(6,2),(6,1),(6,0),(7,0),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),
+                  (9,6),(10,6),(11,6),(12,6),(13,6),(14,6),(14,7),(14,8),(13,8),(12,8),(11,8),(10,8),(9,8),(8,8),(8,9),(8,10),(8,11),(8,12),(8,13),(8,14),
+                  (7,14),(6,14),(6,13),(6,12),(6,11),(6,10),(6,9--nice),(6,8),(5,8),(4,8),(3,8),(2,8),(1,8)]
+goalSquare = [(7,7)]                  
+
+-}
 emptyBoard = Game { gameBoard = (array indexRange $ zip (range indexRange) (cycle [Empty])) // [((0,0), Full PlayerRed),((14,14), Full PlayerBlue)],
                     gamePlayer = PlayerRed,
                     gameState = Running
@@ -52,10 +63,10 @@ emptyBoard = Game { gameBoard = (array indexRange $ zip (range indexRange) (cycl
 -- if the game is still running
 boardAsRunningPicture board = 
     pictures [ color red $ redCellsOfBoard board,
-               color blue $ blueCellsOfBoard board,
+               color blue $ blueCellsOfBoard board,
                color yellow $ yellowCellsOfBoard board,
-               color green $ greenCellsOfBoard board,
-               color black $ boardGrid]
+               color green $ greenCellsOfBoard board,
+               color black $  boardGrid]
 
 -- colors for gameover
 outcomeColor (Just PlayerRed) = red
@@ -135,6 +146,32 @@ gameAsPicture game = translate (fromIntegral screenWidth * (-0.5))
             Running -> boardAsRunningPicture (gameBoard game)
             GameOver winner -> boardAsGameOverPicture winner (gameBoard game)
 
+isCoordCorrect = inRange ((0, 0), (n-1,n-1))
+
+playerSwitch game =
+    case gamePlayer game of
+        PlayerRed -> game {gamePlayer = PlayerGreen}
+        PlayerGreen -> game {gamePlayer = PlayerYellow}
+        PlayerYellow -> game {gamePlayer = PlayerBlue}
+        PlayerBlue -> game {gamePlayer = PlayerRed}
+
+playerTurn :: Game -> (Int, Int) -> Game
+playerTurn game cellCoord
+    | isCoordCorrect cellCoord && board ! cellCoord == Empty = 
+        playerSwitch $ game { gameBoard = board // [(cellCoord, Full $ player)]} 
+    | otherwise = game 
+    where board = gameBoard game
+          player = gamePlayer game 
+
+mousePosCell :: (Float,Float) -> (Int,Int)
+mousePosCell (x,y) = ( floor ((y + (fromIntegral screenHeight * 0.5)) / cellHeight)
+                     , floor ((x + (fromIntegral screenWidth * 0.5)) / cellWidth )
+                     )
+
+transformGame (EventKey(MouseButton LeftButton) Up _ mousePos) game = 
+    case gameState game of 
+        Running -> playerTurn game $ mousePosCell mousePos 
+        GameOver _ -> emptyBoard
 transformGame _ game = game
 
 main :: IO ()
