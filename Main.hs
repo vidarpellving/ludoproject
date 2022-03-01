bce6e2b69b7f065034d7b215dd07a09548e0db3f
Anton Näslund <antonnaslund@student-210-159.eduroam.uu.se>
2022-03-01 15:19:50 +0100

    small fixes

--- Main.hs
+++ Main.hs
@@ -7,9 +7,11 @@ import Graphics.Gloss.Interface.IO.Game (Event(EventKey), MouseButton (LeftButto
 import Graphics.Gloss.Interface.Pure.Game
 import System.Random
 import System.IO
+--import Dice
+import Data.List
 
 --dice
-data Dice = Int deriving Show
+--data Dice = Int deriving Show
 
 --player
 data Player = PlayerRed | PlayerBlue | PlayerYellow | PlayerGreen deriving (Eq, Show)
@@ -23,16 +25,13 @@ data Cell = Empty | Full Player deriving (Eq, Show)
 -- board
 type Board = Array (Int, Int) Cell
 
-
-
-
-
 -- this is a "Record" and creates variables for the type Game 
 -- line 21 is equal, (almost) to data Game = Game Board Player State
 
 data Game = Game { gameBoard :: Board,
                    gamePlayer :: Player,
-                   gameState :: State
+                   gameState :: State,
+                   rnd :: [Float]
                  }
 -- Window
 window = InWindow "Ludo" (600, 600) (100,100)
@@ -56,32 +55,35 @@ cellHeight = fromIntegral screenHeight / fromIntegral n
 n :: Int
 n = 15
 
-{- this is the startboard-}
+--Coordinates and curcial points; validPositions, goalSquare, winColor, entryPoint
+--Total 56 possible positions before winColorRow(which contains 7 positions.)
+validPositions = [(6,2),(6,1),(6,0),(7,0),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(9,6),(10,6),(11,6),
+                  (12,6),(13,6),(14,6),(14,7),(14,8),(13,8),(12,8),(11,8),(10,8),(9,8),(8,8),(8,9),(8,10),(8,11),
+                  (8,12),(8,13),(8,14),(7,14),(6,14),(6,13),(6,12),(6,11),(6,10),(6,9),(6,8),(5,8),(4,8),(3,8),
+                  (2,8),(1,8),(0,8),(0,7),(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(6,5),(6,4),(6,3)]
 
-{-
-validPositions = [(0,8),(0,7),(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(6,5),(6,4),(6,3),(6,2),(6,1),(6,0),(7,0),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),
-                  (9,6),(10,6),(11,6),(12,6),(13,6),(14,6),(14,7),(14,8),(13,8),(12,8),(11,8),(10,8),(9,8),(8,8),(8,9),(8,10),(8,11),(8,12),(8,13),(8,14),
-                  (7,14),(6,14),(6,13),(6,12),(6,11),(6,10),(6,9--nice),(6,8),(5,8),(4,8),(3,8),(2,8),(1,8)]
+
+
 goalSquare = [(7,7)]       
--- Coordinates for crusial points on board
 
 winColorGreen = [(8,1),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6)]
 winColorYellow = [(13,8),(13,7),(12,7),(11,7),(10,7),(9,7),(8,7)]
 winColorBlue = [(6,13),(7,13),(7,12),(7,11),(7,10),(7,9),(7,8)]
 winColorRed = [(1,6),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7)]
 
+entryPointRed = (6,2)    
 entryPointGreen = (12,6)
 entryPointYellow = (8,12)
 entryPointBlue = (2,8)
-entryPointRed = (6,2)
--}
+
+
 --DICE-----------------DICE-----------------DICE-----------------DICE-----------------DICE---------------
-dice :: Int -> IO[Int]
-dice 0 = return []
-dice n = do
-  r  <- randomRIO (1,6)
-  rs <- dice (n-1)
-  return (r:rs)
+-- dice :: Int -> IO[Int]
+-- dice 0 = return []
+-- dice n = do
+--  r  <- randomRIO (1,6)
+--  rs <- dice (n-1)
+--  return (r:rs)
 
 visualDice1 = undefined
 visualDice2 = undefined
@@ -99,7 +101,7 @@ diceR num
     | dice num == [2] = visualDice2
     | dice num == [3] = visualDice3
     | dice num == [4] = visualDice4
-    | dice num == [5] = visualDice5
+    | dice num == [5] = visualDice5 
     | dice num == [6] = visualDice6
 -}
 --DICE-----------------DICE-----------------DICE-----------------DICE-----------------DICE----------------           
@@ -129,9 +131,11 @@ emptyBoard = Game { gameBoard = array indexRange (zip (range indexRange) (repeat
                                                                                                 ((12,3), Full PlayerGreen),
                                                                                                 ((11,2), Full PlayerGreen),
                                                                                                 ((11,3), Full PlayerGreen),
-                                                                                                ((1,6), Full PlayerRed)],
+                                                                                                ((1,6), Full PlayerRed),
+                                                                                                ((6,2), Full PlayerBlue)],
                     gamePlayer = PlayerRed,
-                    gameState = Running
+                    gameState = Running,
+                    rnd = randoms (mkStdGen 42) 
                   }
             -- This is used to define how large the array created will be
             where indexRange = ((0,0), (n-1, n-1))
@@ -140,6 +144,10 @@ emptyBoard = Game { gameBoard = array indexRange (zip (range indexRange) (repeat
     This function displays the different layers when the game is running
     Determines what layers are on the bottom and on the top.
 -}
+
+
+
+
 boardAsRunningPicture board =
     pictures [ color red redCorner,
                color blue blueCorner,
@@ -296,8 +304,8 @@ playerSwitch game =
 
 playerTurn :: Game -> (Int, Int) -> Game
 playerTurn game cellCoord
-    | isCoordCorrect cellCoord && board ! cellCoord == Empty =
-        playerSwitch $ game { gameBoard = board // [(cellCoord, Full player)]}
+    | isCoordCorrect cellCoord && board ! cellCoord == Full player =
+        playerSwitch $ game { gameBoard = board // [(cellCoord, Empty)]}
     | otherwise = game
     where board = gameBoard game
           player = gamePlayer game
@@ -307,11 +315,17 @@ mousePosCell (x,y) = ( floor ((y + (fromIntegral screenHeight * 0.5)) / cellHeig
                      , floor ((x + (fromIntegral screenWidth * 0.5)) / cellWidth )
                      )
 
+transformGame :: Event -> Game -> Game
 transformGame (EventKey(MouseButton LeftButton) Up _ mousePos) game =
     case gameState game of
         Running -> playerTurn game $ mousePosCell mousePos
         GameOver _ -> emptyBoard
 transformGame _ game = game
+
+movePlayer = undefined
+onClick = undefined
+possibleMove = undefined
+
 
 main :: IO ()
 main = play window backgroundColor 30 emptyBoard gameAsPicture transformGame (const id)
